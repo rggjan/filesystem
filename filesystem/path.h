d68802d7351c6a6494410977bb5aa2e14ac6e562
Josh Junon <junon@uber.com>
2016-10-19 22:37:56 -0700

    it was actually a struct

--- filesystem/path.h
+++ filesystem/path.h
@@ -45,7 +45,7 @@ inline bool create_directory(const path&);
  * library at some point in the future.
  */
 class path {
-	friend class std::hash<class filesystem::path>;
+	friend struct std::hash<class filesystem::path>;
 public:
 	typedef std::string value_type;
 
